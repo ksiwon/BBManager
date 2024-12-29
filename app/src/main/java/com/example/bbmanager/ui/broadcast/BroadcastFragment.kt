ee6f23e95859289a3aacc3c942edfaa5e3e1796d
ksiwon <pjo12346@kaist.ac.kr>
2024-12-29 15:42:11 +0900

    score 미구현

--- app/src/main/java/com/example/bbmanager/ui/broadcast/BroadcastFragment.kt
+++ app/src/main/java/com/example/bbmanager/ui/broadcast/BroadcastFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -23,11 +24,14 @@ class BroadcastFragment : Fragment() {
         val scoreLotteTextView: TextView = root.findViewById(R.id.score_lotte)
         val teamLgTextView: TextView = root.findViewById(R.id.team_lg)
         val scoreLgTextView: TextView = root.findViewById(R.id.score_lg)
+        val inningTextView: TextView = root.findViewById(R.id.inning)
+        val bsoImageView: ImageView = root.findViewById(R.id.bso_image)
+        val baseImageView: ImageView = root.findViewById(R.id.base_image)
 
         broadcastViewModel.scoreboardData.observe(viewLifecycleOwner) { scoreboard ->
             val scores = scoreboard.split(" : ")
             if (scores.size == 2) {
-                scoreLotteTextView.text = scores[0].split(" ")[1]
+                scoreLotteTextView.text = scores[0]
                 scoreLgTextView.text = scores[1]
             }
         }
@@ -38,6 +42,24 @@ class BroadcastFragment : Fragment() {
                 teamLotteTextView.text = teams[0]
                 teamLgTextView.text = teams[1]
             }
+        }
+
+        broadcastViewModel.inningData.observe(viewLifecycleOwner) { innings ->
+            inningTextView.text = innings
+        }
+
+        broadcastViewModel.bsoState.observe(viewLifecycleOwner) { bso ->
+            val (b, s, o) = bso
+            val bsoImageName = "bso_${b}_${s}_${o}"
+            val bsoResId = resources.getIdentifier(bsoImageName, "drawable", requireContext().packageName)
+            bsoImageView.setImageResource(bsoResId)
+        }
+
+        broadcastViewModel.baseState.observe(viewLifecycleOwner) { bases ->
+            val (base1, base2, base3) = bases
+            val baseImageName = "base_${base1}_${base2}_${base3}"
+            val baseResId = resources.getIdentifier(baseImageName, "drawable", requireContext().packageName)
+            baseImageView.setImageResource(baseResId)
         }
 
         return root
